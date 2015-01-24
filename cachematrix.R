75a63f451ff627067f2fb1b7772cccccb05421a4
gmcentenom <gmcentenom@gmail.com>
2015-01-24 11:30:43 -0500

    Add Comments
    Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##These functions implement a way to calculate the inverse of a matrix using cached values for
+##improved performance
+
 ## Write a short comment describing this function
+
+##makeCacheMatrix transform a matrix into a object that contains a cached inverse calculation
+##return a list of function that set the values of the matrix, set the cache calculation,
+##return the cache calculation
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,6 +26,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## cacheSolve calculates the inverse in the case there is no cached value 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
